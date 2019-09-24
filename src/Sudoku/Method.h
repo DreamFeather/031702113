0ff6048370af628cb508e42ccb8d39fd83d6a979
Your Name <you@example.com>
2019-09-24 15:54:28 +0800

    second commit

--- src/Sudoku/Method.h
+++ src/Sudoku/Method.h
@@ -2,6 +2,7 @@
 #include<fstream>
 using namespace std;
 void file_output(int **, int);					//输出数独解
-void file_unsolved();						//多阶无解
+void file_unsolved();							//多阶无解
+bool check_float(char *);						//检查浮点数
 void input(int ac, char *av[]);					//读取文件
 void finish();
